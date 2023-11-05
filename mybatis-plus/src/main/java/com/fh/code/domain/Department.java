9a3c627348e51e463e0374b4734d4f07ee7654bd
1678041005 <96185223+1678041005@users.noreply.github.com>
2023-11-05 19:29:37 +0800

    Update Department.java
    增加一个注释

--- mybatis-plus/src/main/java/com/fh/code/domain/Department.java
+++ mybatis-plus/src/main/java/com/fh/code/domain/Department.java
@@ -25,6 +25,11 @@ public class Department implements Serializable {
      */
     private String deptName;
 
+        /**
+     * 单词
+     */
+    private String word;
+
     @TableField(exist = false)
     private static final long serialVersionUID = 1L;
 }
