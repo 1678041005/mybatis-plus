0bc2ba8c1e12730177af90d861482a47fbe06250
yaoyunning <yaoyunning@aliyun.com>
2023-11-05 19:46:18 +0800

    测试提交

---
+++ mybatis-plus/src/main/java/com/fh/code/service/impl/TestServiceImpl.java
@@ -0,0 +1,18 @@
+package com.fh.code.service.impl;
+
+import com.fh.code.service.TestService;
+
+/**
+ * @author hp
+ * @version 1.0
+ * @date 2023/11/5
+ */
+public class TestServiceImpl implements TestService {
+    @Override
+    public void test() {
+        System.out.println("锄禾日当午");
+        System.out.println("汗滴禾下土");
+        System.out.println("谁知盘中餐");
+        System.out.println("粒粒皆辛苦");
+    }
+}
